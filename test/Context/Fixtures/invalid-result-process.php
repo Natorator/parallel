3593324c3b10b712ae884f582bd8612ab90fcebd
Aaron Piotrowski <aaron@trowski.com>
2023-02-17 18:45:47 -0600

    Try another unserializable result

--- test/Context/Fixtures/invalid-result-process.php
+++ test/Context/Fixtures/invalid-result-process.php
@@ -3,10 +3,5 @@
 use Amp\Sync\Channel;
 
 return function (Channel $channel) {
-    return new class {
-        public function __serialize()
-        {
-            throw new Exception("Cannot serialize");
-        }
-    };
+    return fn () => null;
 };
